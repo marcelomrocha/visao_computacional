4b98f6906c7f48c4c8068d379196c3e25e2ce88a
marcelomrocha <marcelo_rocha@midiacom.uff.br>
2021-03-28 16:02:36 -0300

    detectando faces ok! Porem ainda nao finaliza legal

--- captura_e_marca.py
+++ captura_e_marca.py
@@ -8,8 +8,8 @@ cv2.namedWindow("Face", cv2.WINDOW_NORMAL)
 
 while(True):
     conectado, imagem = camera.read()
-    #imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-    facesDetectadas = classificador.detectMultiScale(imagem, scaleFactor=1.5, minSize=(100,100))
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100,100))
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 0, 255), 2)
 
